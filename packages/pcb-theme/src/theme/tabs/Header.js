7dbc3361fa0b2e46142bce2cb4806c5ad7df7b29
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-06-13 23:46:10 -0500

    add base top-bar to Header

--- packages/pcb-theme/src/theme/tabs/Header.js
+++ packages/pcb-theme/src/theme/tabs/Header.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { connect } from 'frontity';
+import React, { useEffect } from "react";
+import { connect } from "frontity";
 
-function Header({ state }) {
-  const data = state.source.get()
+// assets import
+import facebookSVG from '../assets/facebook.svg';
+import yelpSVG from '../assets/yelp.svg';
+
+function Header({ route, state, actions, libraries }) {
+  const menus = state.source.get('menus')
+  console.log(menus);
+
   return (
-    <header>Header</header>
-  )
+    <header>
+      <div>
+        <div>
+          <span>{menus.menuData[0].acf.phone}</span>
+        </div>
+        <div>
+          <span>{menus.menuData[0].acf.address}</span>
+        </div>
+        <div>
+          <object data={facebookSVG} type="image/svg+xml"></object>
+          <object data={yelpSVG} type="image/svg+xml"></object>
+        </div>
+      </div>
+      <div>
+        <div>
+          Logo
+        </div>
+        <nav>
+          Nav
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default connect(Header);
