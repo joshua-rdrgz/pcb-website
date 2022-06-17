f4e416a1715341f17d2c4c3a2933e68d202f638e
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-06-16 22:35:36 -0500

    update App.js to contain Head information as well as basic reset styling

--- packages/pcb-theme/src/theme/App.js
+++ packages/pcb-theme/src/theme/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { connect } from 'frontity';
+import React from "react";
+import { Head, Global, connect, css } from "frontity";
+import * as Variables from "./styles/Variables";
+import Reset from "./styles/Reset";
 
-import Header from './tabs/Header';
-import PageContent from './tabs/PageContent';
-import Footer from './tabs/Footer';
+import Header from "./components/Header";
+import PageContent from "./components/PageContent";
+import Footer from "./components/Footer";
 
 const App = ({ state }) => {
   return (
     <>
+      <Head>
+        <title>Performance Clear Bra - Trusted Protection</title>
+        <meta
+          name="description"
+          content="Performance Clear Bra is the best company for all of your car protection needs!"
+        />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <html lang="en" />
+        <link rel="stylesheet" href="https://use.typekit.net/pjj0xta.css" />
+      </Head>
+      <Global styles={Reset} />
       <Header />
       <PageContent />
       <Footer />
