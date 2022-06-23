19c0d1da0c452d3de573eccab1a7ac41de559bbe
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2022-06-23 16:08:07 -0500

    finish initial styling of Header desktop version

--- packages/pcb-theme/src/theme/components/Header.js
+++ packages/pcb-theme/src/theme/components/Header.js
@@ -1,35 +1,40 @@
 import React from "react";
 import { connect, styled, css } from "frontity";
-import * as Mixins from '../styles/Mixins';
-import * as Variables from '../styles/Variables';
+import * as Mixins from "../styles/Mixins";
+import * as Variables from "../styles/Variables";
 
 import Link from "@frontity/components/link";
-
-// assets import
-import facebookSVG from "../assets/facebook.svg";
-import yelpSVG from "../assets/yelp.svg";
+import pcbLOGO from "../assets/pcb-logo-transparent.svg";
 
 function Header({ route, state, actions, libraries }) {
   // Fetch Menus
   const menus = state.source.get("menus");
   console.log(menus);
 
+  // Fetch Assets
+  const { data: assetData } = state.source.get("media");
+  const assets = Object.values(assetData);
+  const pcbLogo = assets.find((asset) => asset.id === 87);
+  const facebookLogo = assets.find((asset) => asset.id === 82);
+  const yelpLogo = assets.find((asset) => asset.id === 83);
+  console.log(assets);
+
   return (
     <header>
       <TopBarContainer>
-        <CallBlock>
-          {menus.menuData[0].acf.phone}
-        </CallBlock>
-        <AddressBlock>
-          {menus.menuData[0].acf.address}
-        </AddressBlock>
+        <CallBlock>{menus.menuData[0].acf.phone}</CallBlock>
+        <AddressBlock>{menus.menuData[0].acf.address}</AddressBlock>
         <SocialsBlock>
-          <object data={facebookSVG} type="image/svg+xml"></object>
-          <object data={yelpSVG} type="image/svg+xml"></object>
+          <object
+            data={facebookLogo.guid.rendered}
+            type="image/svg+xml"
+          ></object>
+          <object data={yelpLogo.guid.rendered} type="image/svg+xml"></object>
         </SocialsBlock>
       </TopBarContainer>
       <MainBarContainer>
-        <div>Logo</div>
+        {/* <PcbLogo data={pcbLogo.guid.rendered} type="image/svg+xml"></PcbLogo> */}
+        <PcbLogo data={pcbLOGO} type="image/svg+xml"></PcbLogo>
         <Nav>
           <NavList>
             {menus.navLinks
@@ -42,29 +47,36 @@ function Header({ route, state, actions, libraries }) {
                   return (
                     <ParentLink key={link.id} id={link.id}>
                       {link.title.rendered}
-                      <ul>
+                      {/* Font-Awesome drop-down icon here */}
+                      <ChildList>
                         {childItems.map((item) => {
                           return (
                             <ChildLink key={item.id} id={item.id}>
-                              {item.title.rendered}
+                              <StyledLink link={item.url}>
+                                {item.title.rendered}
+                              </StyledLink>
                             </ChildLink>
                           );
                         })}
-                      </ul>
+                      </ChildList>
                     </ParentLink>
                   );
                 }
                 if (link.acf.cta === "yes") {
                   return (
                     <CTA key={link.id} id={link.id}>
-                      <StyledLink link={link.url}>{link.title.rendered}</StyledLink>
+                      <StyledCTA link={link.url}>
+                        {link.title.rendered}
+                      </StyledCTA>
                     </CTA>
                   );
                 }
                 if (link.parent === 0) {
                   return (
                     <NormalLink key={link.id} id={link.id}>
-                      <StyledLink link={link.url}>{link.title.rendered}</StyledLink>
+                      <StyledLink link={link.url}>
+                        {link.title.rendered}
+                      </StyledLink>
                     </NormalLink>
                   );
                 }
@@ -78,69 +90,127 @@ function Header({ route, state, actions, libraries }) {
 
 export default connect(Header);
 
+const PcbLogo = styled.object`
+  width: 250px;
+  margin-left: 3rem;
+`;
 
 const TopBarContainer = styled.div`
   display: flex;
   justify-content: space-around;
-  ${Mixins.addColors(Variables.colorRedDeep2, Variables.colorWhite)}
-  ${Mixins.addHeadingFont(400, 2)}
+  border-bottom: ${Variables.headerBorder};
+  ${Mixins.addColors(Variables.colorRedDeep2, Variables.colorWhite)};
+  ${Mixins.addHeadingFont(400, 2)};
 `;
 
 const CallBlock = styled.div`
   padding-right: 10rem;
-`
+  border-right: ${Variables.headerBorder};
+`;
 
 const AddressBlock = styled.div`
   padding-right: 25rem;
-`
+`;
 
 const SocialsBlock = styled.div`
   display: flex;
   gap: 2rem;
   align-items: center;
-`
+`;
 
 const MainBarContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`
+  align-items: center;
+  background-color: ${Variables.colorBlack};
+  ${Mixins.addHeadingFont(300, 2)}
+`;
 
 const Nav = styled.nav`
-
-`
+  color: ${Variables.colorWhite};
+  margin-right: 7rem;
+  margin-top: 2rem;
+`;
 
 const NavList = styled.ul`
   display: flex;
   justify-content: space-between;
-  gap: 10rem;
-`
+  gap: 8rem;
+`;
 
 const NormalLink = styled.li`
-  background-color: pink;
-  ${Mixins.liCleanUp}
+  ${Mixins.liCleanUp};
+  ${Mixins.addHeadingFont(700, 3)};
 `;
 
 const ParentLink = styled.li`
-  background-color: red;
-  ${Mixins.liCleanUp}
+  ${Mixins.liCleanUp};
+  ${Mixins.addHeadingFont(700, 3)};
+  text-align: center;
+  position: relative;
+  padding-bottom: 2rem;
+  &:hover {
+    color: ${Variables.colorGold};
+  }
+`;
+
+const ChildList = styled.ul`
+  display: none;
+  position: absolute;
+  min-width: 20rem;
+  z-index: 1;
+  margin-left: -6rem;
+  margin-top: 1.5rem;
+  box-shadow: 0 0.5rem 1rem rgba(16, 16, 16, 0.3);
+  background-color: ${Variables.colorGray1};
+  border-radius: 0.5rem;
+  ${ParentLink}:hover & {
+    display: block;
+  }
+  &::before {
+    content: "";
+    display: block;
+    border-color: transparent transparent ${Variables.colorGray1} transparent;
+    border-style: solid;
+    border-width: 1.5rem;
+    position: absolute;
+    top: -3rem;
+    left: 50%;
+    margin-left: -2rem;
+  }
 `;
 
 const ChildLink = styled.li`
-  background-color: purple;
-  ${Mixins.liCleanUp}
-  display: none;
-  ${ParentLink}:hover & {
-    display: block;
+  ${Mixins.liCleanUp};
+  ${Mixins.addHeadingFont(700, 2.5)};
+  text-align: center;
+  &:first-child {
+    padding-top: 0.5rem;
+  }
+  &:last-child {
+    padding-bottom: 1rem;
   }
 `;
 
 const CTA = styled.li`
-  background-color: yellow;
-  ${Mixins.liCleanUp}
-  
+  ${Mixins.liCleanUp};
+  ${Mixins.addHeadingFont(700, 3)};
 `;
 
-
 const StyledLink = styled(Link)`
-  ${Mixins.aCleanUp}
-`
+  ${Mixins.aCleanUp(Variables.colorWhite)};
+  &:hover {
+    color: ${Variables.colorGold};
+  }
+`;
+
+const StyledCTA = styled(Link)`
+  ${Mixins.aCleanUp(Variables.colorGold)};
+  border: 2px solid ${Variables.colorGold};
+  border-radius: 0.5rem;
+  padding: 0.75rem;
+  &:hover {
+    color: ${Variables.colorWhite};
+    border-color: ${Variables.colorRed};
+  }
+`;
--- packages/pcb-theme/src/theme/styles/Mixins.js
+++ packages/pcb-theme/src/theme/styles/Mixins.js
@@ -1,25 +1,29 @@
-import { css } from 'frontity';
-import * as Variables from './Variables'
+import { css } from "frontity";
+import * as Variables from "./Variables";
 
 export const liCleanUp = () => css`
   list-style: none;
   &:hover {
     cursor: pointer;
   }
-`
-export const aCleanUp = () => css`
+`;
+export const aCleanUp = (color) => css`
   text-decoration: none;
-  color: ${Variables.colorWhite}
-`
+  color: ${color};
+  &:link,
+  &:visited {
+    color: ${color};
+  }
+`;
 
 export const addHeadingFont = (fontWeight, fontSize) => css`
   font-family: trade-gothic-next-compressed, sans-serif;
   font-weight: ${fontWeight};
   font-style: normal;
   font-size: ${fontSize}rem;
-`
+`;
 
 export const addColors = (bgColor, txtColor) => css`
   background-color: ${bgColor};
-  color: ${txtColor}
-`
+  color: ${txtColor};
+`;
