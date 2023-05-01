e718ca11f3ea23e2225d35b5b6848377c4c74e37
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-05-01 18:55:43 -0500

    Add Mobile Filter.js stylings

--- packages/pcb-theme/src/theme/blog/archive-components/Filter.js
+++ packages/pcb-theme/src/theme/blog/archive-components/Filter.js
@@ -1,3 +1,46 @@
+import { styled } from "frontity";
+import * as variables from "../styles/variables";
+import dropdown from "../assets/dropdown.svg";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: ${variables.spacing16};
+  padding: ${variables.spacing20};
+  background-color: ${variables.colorNeutral600};
+`;
+
+const FormFieldDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${variables.spacing2};
+`;
+
+const Label = styled.label`
+  color: ${variables.colorNeutral100};
+  ${variables.textBody12};
+  padding: 0 ${variables.spacing4};
+`;
+
+const Select = styled.select`
+  padding: ${variables.spacing8};
+  ${variables.textBody12Bold};
+  border-radius: ${variables.spacing4};
+  border: none;
+  appearance: none;
+  background-image: url(${dropdown});
+  background-repeat: no-repeat;
+  background-size: ${variables.spacing24};
+  background-position: right;
+  &:focus {
+    outline-color: ${variables.colorPrimary700};
+  }
+`;
+
+const Option = styled.option`
+  ${variables.textBody12};
+`;
+
 const Filter = ({ filterState }) => {
   const { setSelectedCategory } = filterState.category;
   const { setSelectedTag } = filterState.tag;
@@ -16,27 +59,30 @@ const Filter = ({ filterState }) => {
   };
 
   return (
-    <form>
-      <label htmlFor="category">Type of Article</label>
-      <select
-        name="category"
-        id="category"
-        onChange={(e) => onChangeHandler(e, "category")}
-      >
-        <option value="All">All</option>
-        <option value="Article">Article</option>
-        <option value="News">News</option>
-        <option value="Case Study">Case Study</option>
-      </select>
-      <br />
-      <label htmlFor="tag">Type of Service</label>
-      <select name="tag" id="tag" onChange={(e) => onChangeHandler(e, "tag")}>
-        <option value="All">All</option>
-        <option value="Paint Protection Film">Paint Protection Film</option>
-        <option value="Window Tint">Window Tint</option>
-        <option value="Ceramic Coating">Ceramic Coating</option>
-      </select>
-    </form>
+    <Form>
+      <FormFieldDiv>
+        <Label htmlFor="category">Type of Article</Label>
+        <Select
+          name="category"
+          id="category"
+          onChange={(e) => onChangeHandler(e, "category")}
+        >
+          <Option value="All">All</Option>
+          <Option value="Article">Article</Option>
+          <Option value="News">News</Option>
+          <Option value="Case Study">Case Study</Option>
+        </Select>
+      </FormFieldDiv>
+      <FormFieldDiv>
+        <Label htmlFor="tag">Type of Service</Label>
+        <Select name="tag" id="tag" onChange={(e) => onChangeHandler(e, "tag")}>
+          <Option value="All">All</Option>
+          <Option value="Paint Protection Film">Paint Protection Film</Option>
+          <Option value="Window Tint">Window Tint</Option>
+          <Option value="Ceramic Coating">Ceramic Coating</Option>
+        </Select>
+      </FormFieldDiv>
+    </Form>
   );
 };
 
