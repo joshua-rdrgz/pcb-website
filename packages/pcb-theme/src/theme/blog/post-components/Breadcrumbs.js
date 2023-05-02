77dbd6d1564f0a4124ecdbd4e51521fef796bb8f
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-05-02 17:44:34 -0500

    Add styling to Breadcrumbs.js

--- packages/pcb-theme/src/theme/blog/post-components/Breadcrumbs.js
+++ packages/pcb-theme/src/theme/blog/post-components/Breadcrumbs.js
@@ -1,13 +1,50 @@
-import React from "react";
+import { styled } from "frontity";
 import Link from "@frontity/components/link";
+import * as variables from "../styles/variables";
+
+const Div = styled.div`
+  text-align: center;
+  color: ${variables.colorNeutral600};
+  display: flex;
+  justify-content: center;
+  padding: ${variables.spacing8} 0;
+  ${variables.textBody12};
+  @media (min-width: ${variables.breakpoint600}) {
+    padding: ${variables.spacing12} 0;
+    ${variables.textBody16Bold}
+  }
+`;
+
+const Span = styled.span`
+  background-color: ${variables.colorNeutral300};
+  padding: ${variables.spacing4} ${variables.spacing8};
+  border-radius: ${variables.spacing2};
+`;
+
+const FrontityLink = styled(Link)`
+  text-decoration: none;
+  color: ${variables.colorNeutral600};
+  cursor: pointer;
+`;
+
+const HighlightedFrontityLink = styled(Link)`
+  text-decoration: none;
+  color: ${variables.colorNeutral900};
+  cursor: pointer;
+`;
 
 const Breadcrumbs = ({ postData }) => {
   const { postLink, postTitle } = postData;
   return (
-    <div>
-      <Link link="/">Home</Link> / <Link link="/blog">Blog</Link> /{" "}
-      <Link link={postLink}>{postTitle}</Link>
-    </div>
+    <Div>
+      <Span>
+        <FrontityLink link="/">Home</FrontityLink> /{"   "}
+        <FrontityLink link="/blog">Blog</FrontityLink> /{"   "}
+        <HighlightedFrontityLink link={postLink}>
+          {postTitle}
+        </HighlightedFrontityLink>
+      </Span>
+    </Div>
   );
 };
 
