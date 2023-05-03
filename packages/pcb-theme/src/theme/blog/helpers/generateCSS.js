6f42e8070edf1360393f74ace5395db945d7e37a
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-05-03 15:38:57 -0500

    Add "!F" and "!M####" functionality to generateCSS

--- packages/pcb-theme/src/theme/blog/helpers/generateCSS.js
+++ packages/pcb-theme/src/theme/blog/helpers/generateCSS.js
@@ -1,27 +1,60 @@
 const filterForCustomCSSClasses = (classes) => {
   if (classes) {
-    const customClasses = classes
-      .split(" ")
-      .filter((cssClass) => cssClass.startsWith("!C"));
+    const classesArray = classes.split(" ");
+    const generalRulesets = classesArray.filter((cssClass) =>
+      cssClass.startsWith("!C")
+    );
+    const isFlexClass = classesArray.some((rule) => rule === "!F");
+    const customMediaQueryRulesets = classesArray.filter((cssClass) =>
+      cssClass.startsWith("!M")
+    );
 
-    return customClasses.length > 0
-      ? // *
-        // SLICE REMOVES "!C" FROM RULESET
-        // *
-        customClasses.map((customClass) => customClass.slice(2))
-      : null;
+    // *
+    // SLICE REMOVES "!C" or "!M" FROM RULESET
+    // *
+    return {
+      generalRulesets: generalRulesets?.map((rule) => rule.slice(2)),
+      mediaQueryRulesets: customMediaQueryRulesets?.map((rule) =>
+        rule.slice(2)
+      ),
+      isFlexClass,
+    };
   }
   return null;
 };
 
 const generateCSS = (classes) => {
-  const customClasses = filterForCustomCSSClasses(classes);
-  return customClasses
-    ?.map((customClass) => {
+  const filteredClasses = filterForCustomCSSClasses(classes);
+
+  if (filteredClasses) {
+    const { generalRulesets, mediaQueryRulesets, isFlexClass } =
+      filteredClasses;
+
+    const generalRules = generalRulesets?.map((customClass) => {
       const [property, value] = customClass.split("&");
       return `${property}: ${value};`;
-    })
-    .join(" ");
+    });
+
+    const mediaQueryRules = mediaQueryRulesets?.map((mediaQueryRuleset) => {
+      const [breakpoint, property, value] = mediaQueryRuleset.split("&");
+      return `@media (min-width: ${
+        +breakpoint / 16
+      }em) {${property}: ${value};}`;
+    });
+
+    const flexRules = `display: flex; flex-direction: column;`;
+
+    const rulesets = generalRules.concat(mediaQueryRules);
+
+    if (isFlexClass) {
+      rulesets.push(flexRules);
+      return rulesets.join(" ");
+    }
+
+    return rulesets.join(" ");
+  }
+
+  return null;
 };
 
 export default generateCSS;
