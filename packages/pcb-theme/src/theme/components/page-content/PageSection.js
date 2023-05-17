9f7dbb7fef525d8d001c37b32256ead8541ccc27
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-05-16 20:38:37 -0500

    Site Refactor: Basic Page Section Processor Setup

---
+++ packages/pcb-theme/src/theme/components/page-content/PageSection.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { connect } from "frontity";
+
+const PageSection = () => {
+  return <div>PageSection</div>;
+};
+
+export default connect(PageSection);
---
+++ packages/pcb-theme/src/theme/processors/pageSectionProcessor.js
@@ -0,0 +1,23 @@
+import PageSection from "../components/page-content/PageSection";
+import gatherSectionContent from "../helpers/gatherSectionContent";
+
+const pageSectionProcessor = {
+  name: "Page Section Processor",
+  priority: 10,
+  test: ({ node }) => {
+    return node?.props?.className?.includes("wp-block-group" && "page-section");
+  },
+  processor: ({ node }) => {
+    const sectionContent = gatherSectionContent(node);
+
+    console.log(sectionContent);
+
+    return {
+      component: PageSection,
+      props: {
+        content: sectionContent,
+        tag: "section",
+      },
+    };
+  },
+};
