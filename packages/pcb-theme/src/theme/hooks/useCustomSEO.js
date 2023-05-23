2f36be6f33e06da717865e625d2b7514ba9acc9e
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-05-23 01:20:52 -0500

    Get rid of debugging logs

--- packages/pcb-theme/src/theme/hooks/useCustomSEO.js
+++ packages/pcb-theme/src/theme/hooks/useCustomSEO.js
@@ -3,18 +3,14 @@ import { useEffect } from "react";
 const useCustomSEO = () => {
   useEffect(() => {
     const isWpDomain = window.location.host.includes('wp.performanceclearbra.com');
-    console.log('host: ', window.location.host);
-    console.log('isWPDomain: ', isWpDomain);
 
     if (isWpDomain) {
       const metaTags = document.head.querySelectorAll('meta[name="robots"]');
-      console.log(metaTags);
 
       // Remove existing "index" directive meta tags
       metaTags.forEach((tag) => {
         const content = tag.getAttribute('content');
         if (content && content.toLowerCase().includes("index")) {
-          console.log('we`ve found an index tag, time to remove.');
           tag.parentNode.removeChild(tag);
         }
       });
