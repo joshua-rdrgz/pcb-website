55f8fc467050c376291194d0359b3311f78ff284
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-05-31 17:59:55 -0500

    App includes base SEO Schema

--- packages/pcb-theme/src/theme/App.js
+++ packages/pcb-theme/src/theme/App.js
@@ -3,6 +3,7 @@ import useCustomSEO from "./hooks/useCustomSEO";
 import { Head, Global, connect, loadable } from "frontity";
 import Switch from "@frontity/components/switch";
 import Reset from "./styles/Reset";
+import gatherFeaturedImage from "./helpers/gatherFeaturedImage";
 
 const Archive = loadable(() => import("./blog/Archive"));
 const Post = loadable(() => import("./blog/Post"));
@@ -22,10 +23,11 @@ const LandingFooter = loadable(() =>
 
 const App = ({ state }) => {
   const data = state.source.get(state.router.link);
-
   const isLandingPage = data.isLandingPage ? data.isLandingPage : false;
 
-  useCustomSEO();
+  const featuredImage = gatherFeaturedImage(state, data);
+
+  const { isCoreSitePage } = useCustomSEO();
 
   return (
     <>
@@ -40,6 +42,37 @@ const App = ({ state }) => {
           href={state.theme.favicon.guid?.rendered}
         />
         <link rel="stylesheet" href="https://use.typekit.net/zau4ika.css" />
+        {isCoreSitePage && (
+          <script type="application/ld+json">
+            {`
+            {
+              "@context": "http://www.schema.org",
+              "@type": "AutomotiveBusiness",
+              "name": "Performance Clear Bra",
+              "url": "https://performanceclearbra.com${state.router.link}",
+              "priceRange": "$$$$",
+              "image": "${featuredImage}",
+              "description": "Performance Clear Bra is the number 1 shop in Fort Worth, Texas for all of your paint protection film installation needs.",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "2800 Shamrock Ave., Suite 116",
+                "addressLocality": "Fort",
+                "addressRegion": "Texas",
+                "postalCode": "76107",
+                "addressCountry": "USA"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": "32.764070916053754",
+                "longitude": "-97.35543553105796"
+              },
+              "hasMap": "https://goo.gl/maps/Qbx4QyoywH4dg9N2A",
+              "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+              "telephone": "(972)295-7068"
+            }
+          `}
+          </script>
+        )}
       </Head>
       <Global styles={Reset} />
       {isLandingPage ? <LandingHeader /> : <Header />}
