1c3adc14e1f5d99eda6db3036ff4f3d04ff66cbb
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-06-01 20:24:31 -0500

    Edit media handler to allow up to 200 images.

--- packages/pcb-theme/src/theme/handlers/media-handler.js
+++ packages/pcb-theme/src/theme/handlers/media-handler.js
@@ -1,21 +1,30 @@
 const mediaHandler = {
-  name: 'media',
+  name: "media",
   priority: 10,
-  pattern: 'media/',
+  pattern: "media/",
   func: async ({ route, state, libraries }) => {
     const { api } = libraries.source;
-    const response = await api.get({
+    const response1 = await api.get({
       endpoint: "media",
       params: {
-        per_page: 100
+        per_page: 100,
       },
     });
-    const items = await response.json();
+    const response2 = await api.get({
+      endpoint: "media",
+      params: {
+        per_page: 100,
+        offset: 100,
+      },
+    });
+    const items1 = await response1.json();
+    const items2 = await response2.json();
+    const items = [...items1, ...items2];
     const currentPageData = state.source.data[route];
     Object.assign(currentPageData, {
       data: items,
     });
-  }
+  },
 };
 
 export default mediaHandler;
